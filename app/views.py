11d6ab6fd5cb2136fe9c9e8c874275fded67688e
Carlator <clorenzozuniga@gmail.com>
2023-05-10 22:10:18 +0200

    Optimised graph rendering, no need for images.

--- app/views.py
+++ app/views.py
@@ -5,18 +5,26 @@ from solarkit import utils
 import matplotlib
 
 import matplotlib.pyplot as plt
+from io import StringIO
+
 
 # Create your views here.
 def index(request):
     return render(request, "app/index.html")
 
 def orbits_default(request):
-    matplotlib.use("Agg")
-    viewer = Viewer(system=utils.load_system_from_csv("app\data\planet_data.csv"), planets_to_use=["Earth", "Venus", "Mars"])
+    context = {}
+    
+    
+    viewer = Viewer(system=utils.load_system_from_csv("app\data\planet_data.csv"), planets_to_use=["Mercury", "Venus", "Earth", "Mars"], compute_3D=True)
+    viewer.server_mode()
     viewer.initialise_plotter()
-    viewer.heliocentric_model(origin_planet_name="Earth")
+    viewer.system_orbits()
     viewer.add_grid()
     viewer.add_legend()
-    viewer.save_figure(path="app\static\images", filename="orbits")
-    plt.close()
-    return render(request, "app/orbits.html")
+    
+    
+    context["graph"] = viewer.get_figure_data
+    
+    
+    return render(request, "app/orbits.html", context)
