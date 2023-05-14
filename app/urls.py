9072b003ec0232d8127f1542710d98245bfb8f5f
Carlator <clorenzozuniga@gmail.com>
2023-05-14 12:05:00 +0200

    - Website working

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index),
     path("orbits/", views.orbits),
+    path("spinograph/", views.spinograph),
+    path("heliocentric/", views.heliocentric),
+    path("third-law/", views.third_law),
 ]
--- app/views.py
+++ app/views.py
@@ -3,28 +3,32 @@ from django.shortcuts import render
 from solarkit.viewer import Viewer
 from solarkit import utils
 
-from .forms import Viewer_Form
-from .models import Viewer_Data
 
 
-def create_viewer_from_object() -> Viewer:
-    """
-    Fetches Viewer_Data object to create solarkit.Viewer object
+def create_viewer_from_querydict(query_dict: dict) -> Viewer:
+    """_summary_
+    Creates a solarkit.Viewer object from request
+
+    
+    Args:
+        query_dict (dict): query dictionary
 
     Returns:
-        Viewer: solarkit.Viewer using Viewer_Data as arguments
+        Viewer: solarkit.Viewer using request as arguments
     """
+    query_dict = {key: True for key, value in query_dict.items() if value}
     
-    data = Viewer_Data.objects.all().values()[0]
+    planets_to_use = [key.capitalize() for key, value in query_dict.items() if value and key != "compute_3D"]
     
-    compute_3D = data["compute_3D"]
-    
-    planets_to_use = [key.capitalize() for key, value in data.items() if value == True and key != "compute_3D"]
-    
+    if "compute_3D" in query_dict.keys():
+        compute_3D = True
+    else:
+        compute_3D = False
+        
     system = utils.load_system_from_csv("app\data\planet_data.csv")
     
     return Viewer(system=system, planets_to_use=planets_to_use, compute_3D=compute_3D)
-    
+
 
 # Create your views here.
 def index(request):
@@ -32,29 +36,71 @@ def index(request):
     return render(request, "app/index.html")
 
 def orbits(request):
-    form = Viewer_Form()
-    context = {"form": form}
     
-    if request.method == "POST":
-        if len(Viewer_Data.objects.all()) != 0:
-            Viewer_Data.objects.all().delete()
-        
-        form = Viewer_Form(request.POST)
-        
-        if form.is_valid():
-            form.save()
-        
-        create_viewer_from_object()
+    context = {}
     
-    viewer = create_viewer_from_object()
+    if request.GET:
+        viewer = create_viewer_from_querydict(query_dict=request.GET)
+    
+        viewer.server_mode()
+        viewer.initialise_plotter()
+        viewer.system_orbits()
+        viewer.add_grid()
+        viewer.add_legend()
+        
+        context["graph"] = viewer.get_figure_data()
+    
+    
+    return render(request, "app/orbits.html", context)
+
+
+def spinograph(request):
+    
+    context = {}
+    
+    if request.GET:
+        viewer = create_viewer_from_querydict(query_dict=request.GET)
+    
+        viewer.server_mode()
+        viewer.initialise_plotter()
+        viewer.spinograph()
+        viewer.add_grid()
+        viewer.add_legend()
+        
+        context["graph"] = viewer.get_figure_data()
+    
+    
+    return render(request, "app/spinograph.html", context)
+
+def heliocentric(request):
+    context = {}
+    
+    if request.GET:
+        viewer = create_viewer_from_querydict(query_dict=request.GET)
+    
+        viewer.server_mode()
+        viewer.initialise_plotter()
+        viewer.heliocentric_model(origin_planet_name="Earth")
+        viewer.add_grid()
+        viewer.add_legend()
+        
+        context["graph"] = viewer.get_figure_data()
+    
+    
+    return render(request, "app/heliocentric.html", context)
+    
+def third_law(request):
+    context = {}
+    system = utils.load_system_from_csv("app\data\planet_data.csv")
+    viewer = Viewer(system=system)
+
     viewer.server_mode()
     viewer.initialise_plotter()
-    viewer.system_orbits()
+    viewer.third_law()
     viewer.add_grid()
     viewer.add_legend()
-    
     
     context["graph"] = viewer.get_figure_data()
     
     
-    return render(request, "app/orbits.html", context)
+    return render(request, "app/third_law.html", context)
